c7a9aa9caf7449ab6fca494397492b5dbf799545
0x1579FEE9 <0x1579FEE9@user.noreply.gitee.com>
2026-08-02 19:26:26 +0800

    chore: icp

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     outline: "deep",
     sidebar: [],
     socialLinks: [{ icon: "github", link: "https://github.com/0x1579FEE9" }],
+    footer: {
+      message: `<a href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">粤ICP备2026007027号-2</a>`,
+    },
   },
   rewrites: {
     "readme.md": "index.md",
