8a95b8681ef406a0ef51789bcdb603371422a443
0x1579FEE9 <0x1579FEE9@user.noreply.gitee.com>
2026-08-14 01:15:11 +0800

    feat: use pagefind

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vitepress";
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
+
+function chineseSearchOptimize(input: string) {
+  const segmenter = new Intl.Segmenter('zh-CN', { granularity: 'word' })
+  const result: string[] = []
+  for (const it of segmenter.segment(input)) {
+    if (it.isWordLike) {
+      result.push(it.segment)
+    }
+  }
+  return result.join(' ')
+}
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: "zh-cn",
   title: "PasteBin",
   lastUpdated: true,
   cleanUrls: true,
@@ -16,4 +29,9 @@ export default defineConfig({
   rewrites: {
     "readme.md": "index.md",
   },
+  vite: {
+    plugins: [pagefindPlugin({
+      customSearchQuery: chineseSearchOptimize
+    })],
+  }
 });
--- readme.md
+++ readme.md
@@ -1,8 +1,24 @@
 # 0x1579FEE9
 
-个人 PasteBin
+个人 PasteBin。
+
+以解决问题为主，不涉及具体技术细节原理的讨论，提供的代码片段和问题解决步骤不一定是最佳的解决方法。
+
+## clangd 在使用 mingw 的时候找不到（或没有正确的）stdio.h
+
+`#C` `#clangd`
+
+Windows 下 clangd 默认目标是 `windows-msvc`。添加 `.clangd` 手动指定为 mingw。
+
+```yaml
+CompileFlags:
+  Add:
+    - --target=x86_64-w64-mingw32
+```
 
 ## Hello World
+
+`#C` `#test`
 
 Hello World
 ```c
